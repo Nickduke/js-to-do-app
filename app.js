94429a969a6920202afa593b140544bdcd440e7a
nickNtoukas <ntoukas.nick@gmail.com>
2022-05-11 14:40:07 -0400

    Added js file with pseudo code

---
+++ app.js
@@ -0,0 +1,23 @@
+// Add a submit event listener on the form
+const formElement = document.querySelector('form');
+formElement.addEventListener('submit', () => {
+  console.log('form is submitted');
+});
+
+// Stop the page from refreshing when the form is submitted
+
+// Only if the user has entered an actual task (AKA input is not empty):
+
+// Grab the user's to-do item from the form input
+// Display to do on page within an li element
+// Include  a checkbox icon within the li
+// Add li element to the ul
+
+// Clicking on a task allows you to toggle between checked/unchecked (AKA done vs not done)
+
+// BONUS LEVEL:
+// Add a "reset" button which clears all of the to - dos
+// Add a "remove task" button to each task
+// Add a edit task button
+// Add congrats alert when all of the existing to-dos are checked off
+// Take a break message if 5 or more tasks completed
